a4ad22af8eb2ac068bf1813addca16b90df2710c
AlexBocken <alexander@bocken.org>
2023-06-25 12:15:20 +0200

    Image parallax on recipes

---
+++ src/routes/test/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+import TitleImgParallax from "$lib/components/TitleImgParallax.svelte";
+</script>
+<style>
+div{
+	transform: translateY(-1rem);
+}
+</style>
+<div>
+<TitleImgParallax></TitleImgParallax>
+</div>
