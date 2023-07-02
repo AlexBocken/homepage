24ddd39f35f8fb918babde5af84587ef3a578134
AlexBocken <alexander@bocken.org>
2023-07-02 23:39:31 +0200

    API routes now return proper Responses and basic errors are handled slight improvements in layouting

--- src/routes/api/delete/+server.ts
+++ src/routes/api/delete/+server.ts
@@ -3,6 +3,7 @@ import { Recipe } from '../../../models/Recipe';
 import { dbConnect, dbDisconnect } from '../../../utils/db';
 import type {RecipeModelType} from '../../../types/types';
 import { BEARER_TOKEN } from '$env/static/private'
+import { error } from '@sveltejs/kit';
 // header: use for bearer token for now
 // recipe json in body
 export const POST: RequestHandler = async ({request}) => {
@@ -10,14 +11,14 @@ export const POST: RequestHandler = async ({request}) => {
   const short_name = message.old_short_name
   const bearer_token = message.headers.bearer
   if(bearer_token === BEARER_TOKEN){
-	console.log("PASSWORD CORRECT")
   	await dbConnect();
 	await Recipe.findOneAndDelete({short_name: short_name});
   	await dbDisconnect();
-	return {status: 400} //TODO: cleanup error throwing
+	return new Response(JSON.stringify({msg: "Deleted recipe successfully"}),{
+			    status: 200,
+  	});
   }
   else{
-	console.log("PASSWORD INCORRECT")
-	  return {status: 403}
+	throw error(403, "Password incorrect")
   }
-};
+}
