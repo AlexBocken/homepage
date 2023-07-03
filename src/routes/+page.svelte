5254d840d439d0cd529191b582419d41ca9de7b8
AlexBocken <alexander@bocken.org>
2023-07-03 18:20:58 +0200

    change place of photoprism

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,22 @@
 <script>
 	import Header from '$lib/components/Header.svelte'
 </script>
+<style>
+</style>
+
 <Header>
 <ul class=site_header slot=links>
 	<li><a href="/rezepte">Rezepte</a></li>
-	<li><a href="https://bilder.bocken.org">Familienbilder</a></li>
+	<li><a href="/bilder">Bilder</a></li>
 </ul>
 
+
+<section>
+<h2><a href="/rezepte">Rezepte</a></h2>
+</section>
+
+<section>
+<h2><a href="/bilder">Bilder</a></h2>
+</section>
 
 </Header>
