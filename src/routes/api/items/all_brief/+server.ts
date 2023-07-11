50c17b2a2faaad8617cceae9d4d6360ce8619b1d
AlexBocken <alexander@bocken.org>
2023-07-11 22:54:13 +0200

    randomize moved to API

--- src/routes/api/items/all_brief/+server.ts
+++ src/routes/api/items/all_brief/+server.ts
@@ -2,10 +2,11 @@ import { json, type RequestHandler } from '@sveltejs/kit';
 import type { BriefRecipeType } from '../../../../types/types';
 import { Recipe } from '../../../../models/Recipe'
 import { dbConnect, dbDisconnect } from '../../../../utils/db';
+import { rand_array } from '$lib/js/randomize';
 
 export const GET: RequestHandler = async ({params}) => {
   await dbConnect();
-  let found_brief = (await Recipe.find({}, 'name short_name tags category icon description season').lean()) as BriefRecipeType[];
+  let found_brief = rand_array(await Recipe.find({}, 'name short_name tags category icon description season').lean()) as BriefRecipeType[];
   await dbDisconnect();
   return json(JSON.parse(JSON.stringify(found_brief)));
 };
--- src/routes/api/items/in_season/[month]/+server.ts
+++ src/routes/api/items/in_season/[month]/+server.ts
@@ -1,10 +1,12 @@
+import type {rand_array} from '$lib/js/randomize';
 import { json, type RequestHandler } from '@sveltejs/kit';
 import { Recipe } from '../../../../../models/Recipe'
 import { dbConnect, dbDisconnect } from '../../../../../utils/db';
+import { rand_array } from '$lib/js/randomize';
 
 export const GET: RequestHandler = async ({params}) => {
   await dbConnect();
-  let found_in_season = (await Recipe.find({season: params.month}, 'name short_name images tags category icon description season').lean());
+  let found_in_season = rand_array(await Recipe.find({season: params.month}, 'name short_name images tags category icon description season').lean());
   await dbDisconnect();
   found_in_season = JSON.parse(JSON.stringify(found_in_season));
   return json(found_in_season);
