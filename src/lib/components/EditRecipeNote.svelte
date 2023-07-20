58d264390851233727c6b73d6a03e8b415c2f0d6
AlexBocken <alexander@bocken.org>
2023-07-21 00:18:37 +0200

    add Note field in recipe

---
+++ src/lib/components/EditRecipeNote.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+</script>
+<style>
+div{
+	background-color: var(--red);
+	color: white;
+	padding: 1em;
+	font-size: 1.1rem;
+	max-width: 400px;
+	margin-inline: auto;
+	box-shadow: 0.2em 0.2em 0.5em 0.1em rgba(0, 0, 0, 0.3);
+	margin-bottom: 1em;
+}
+h3{
+	margin-block: 0;
+}
+</style>
+
+<div>
+	<h3>Notiz:</h3>
+	<slot></slot>
+</div>
