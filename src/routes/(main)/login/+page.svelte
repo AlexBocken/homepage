4c92c1c43da48f2c63b1bd510601779542c109b5
Alexander Bocken <alexander@bocken.org>
2024-02-03 13:03:23 +0100

    add note about recipes login

--- src/routes/(main)/login/+page.svelte
+++ src/routes/(main)/login/+page.svelte
@@ -4,6 +4,11 @@ import "$lib/css/nordtheme.css"
 </script>
 <form action="?/login" method=POST>
 	<h1>Log In</h1>
+	<p>
+		Note: This is a separate account to all the services running on this server.
+		Please only use this login if you want to edit recipes.
+		In the future, this login will be merged with the other services.
+	</p>
 	<label>
 		Benutzername
 		<input type="text" name="username" required>
