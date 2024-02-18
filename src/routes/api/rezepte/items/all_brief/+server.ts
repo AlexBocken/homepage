d5228aab603c323ca87a69ee7ec3e33bca6cf1a6
Alexander Bocken <alexander@bocken.org>
2024-02-18 14:45:01 +0100

    add forgotten updated api routes

--- src/routes/api/rezepte/items/all_brief/+server.ts
+++ src/routes/api/rezepte/items/all_brief/+server.ts
@@ -6,7 +6,7 @@ import { rand_array } from '$lib/js/randomize';
 
 export const GET: RequestHandler = async ({params}) => {
   await dbConnect();
-  let found_brief = rand_array(await Recipe.find({}, 'name short_name tags category icon description season').lean()) as BriefRecipeType[];
+  let found_brief = rand_array(await Recipe.find({}, 'name short_name tags category icon description season dateModified').lean()) as BriefRecipeType[];
   await dbDisconnect();
   return json(JSON.parse(JSON.stringify(found_brief)));
 };
