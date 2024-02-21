8b5e089792a9d8c691e311d74bab1e4b5ddc85f6
Alexander Bocken <alexander@bocken.org>
2024-02-21 10:29:17 +0100

    attempt to fix rand_array()

--- src/lib/js/randomize.js
+++ src/lib/js/randomize.js
@@ -1,6 +1,4 @@
-const time = new Date()
 const MS_PER_DAY = 86400000
-let seed = Math.floor(time.getTime()/MS_PER_DAY)
 function mulberry32(a) {
 	return function() {
 	  var t = a += 0x6D2B79F5;
@@ -11,6 +9,8 @@ function mulberry32(a) {
 }
 
 export function rand_array(array){
+	let time = new Date()
+	const seed = Math.floor(time.getTime()/MS_PER_DAY)
 	let rand = mulberry32(seed)
 	array.sort((a,b) => 0.5 - rand())
 	return array
