87d5e9cbc06b47070140e2eecc5a779c4c92f2b6
Alexander Bocken <alexander@bocken.org>
2026-02-01 14:10:52 +0100

    rosary: clean up unused CSS, fix unclosed tag
    - FireEffect now only contains fire-related styles
    - StreakAura now only contains aura, number, halo, wing styles
    - Fix unclosed <i> tags in JosephGebet.svelte

--- src/lib/components/prayers/JosephGebet.svelte
+++ src/lib/components/prayers/JosephGebet.svelte
@@ -1,6 +1,6 @@
 <p>
-	<v>Jungfräulicher Vater <i><sup>⚬</sup>Jesu,</v>
-	<v>Reinster Bräutigam <i><sup>⚬</sup>Mariä,</v>
+	<v>Jungfräulicher Vater <i><sup>⚬</sup></i>Jesu,</v>
+	<v>Reinster Bräutigam <i><sup>⚬</sup></i>Mariä,</v>
 	<v>Sankt Joseph, bitte Tag für Tag bei Jesus, dem Sohn Gottes.</v>
 	<v>Seine Kraft und Gnade soll uns stärken,</v>
 	<v>dass wir siegreich streiten im Leben</v>
