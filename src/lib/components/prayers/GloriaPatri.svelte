95d3be8b82b2d3c3975a49277c1166d836b371a9
Alexander Bocken <alexander@bocken.org>
2026-02-02 20:39:11 +0100

    prayers: bow-emphasis only for main language

--- src/lib/components/prayers/GloriaPatri.svelte
+++ src/lib/components/prayers/GloriaPatri.svelte
@@ -4,9 +4,9 @@
 
 <Prayer>
 	<p>
-		<v lang="la">Glória Patri, et Fílio, et Spirítui Sancto.</v>
-		<v lang="de">Ehre sei dem Vater und dem Sohne und dem Hl. Geiste.</v>
-		<v lang="en">Glory be to the Father, and to the Son, and to the Holy Spirit.</v>
+		<v lang="la"><i><sup>⚬</sup></i>Glória Patri, et Fílio, et Spirítui Sancto.</v>
+		<v lang="de"><i><sup>⚬</sup></i>Ehre sei dem Vater und dem Sohne und dem Hl. Geiste.</v>
+		<v lang="en"><i><sup>⚬</sup></i>Glory be to the Father, and to the Son, and to the Holy Spirit.</v>
 		<v lang="la">Sicut erat in princípio, et nunc, et semper:</v>
 		<v lang="de">Wie es war am Anfang, so auch jetzt und allezeit</v>
 		<v lang="en">As it was in the beginning, is now, and ever shall be,</v>
