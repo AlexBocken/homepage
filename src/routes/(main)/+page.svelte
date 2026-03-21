e189379d556c77920573caa7b2c66e313851a6de
Alexander Bocken <alexander@bocken.org>
2026-03-21 10:40:32 +0100

    fitness: rename to "Fitness" on homepage

--- src/routes/(main)/+page.svelte
+++ src/routes/(main)/+page.svelte
@@ -56,7 +56,7 @@
 		transmission: 'Transmission',
 		documents: isEnglish ? 'Documents' : 'Dokumente',
 		audiobooksPodcasts: isEnglish ? 'Audiobooks & Podcasts' : 'Hörbücher & Podcasts',
-		gym: 'Gym'
+		fitness: 'Fitness'
 	});
 </script>
 <style>
@@ -224,7 +224,7 @@ section h2{
 	<a href="/fitness">
 		<svg class="lock-icon"><use href="#lock-icon"/></svg>
 		<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512"><!--!Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M96 64c0-17.7 14.3-32 32-32l32 0c17.7 0 32 14.3 32 32l0 160 0 64 0 160c0 17.7-14.3 32-32 32l-32 0c-17.7 0-32-14.3-32-32l0-64-32 0c-17.7 0-32-14.3-32-32l0-64c-17.7 0-32-14.3-32-32s14.3-32 32-32l0-64c0-17.7 14.3-32 32-32l32 0 0-64zm448 0l0 64 32 0c17.7 0 32 14.3 32 32l0 64c17.7 0 32 14.3 32 32s-14.3 32-32 32l0 64c0 17.7-14.3 32-32 32l-32 0 0 64c0 17.7-14.3 32-32 32l-32 0c-17.7 0-32-14.3-32-32l0-160 0-64 0-160c0-17.7 14.3-32 32-32l32 0c17.7 0 32 14.3 32 32zM416 224l0 64-192 0 0-64 192 0z"/></svg>
-		<h3>{labels.gym}</h3>
+		<h3>{labels.fitness}</h3>
 	</a>
 </LinksGrid>
 </section>
