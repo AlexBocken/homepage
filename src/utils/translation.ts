a580e4b91e3bca29b585fe0ce89020b55b510a8f
Alexander Bocken <alexander@bocken.org>
2026-03-23 07:46:34 +0100

    replace ß with ss for Swiss High German throughout codebase

--- src/utils/translation.ts
+++ src/utils/translation.ts
@@ -635,7 +635,7 @@ class DeepLTranslationService {
 			.replace(/ä/g, 'ae')
 			.replace(/ö/g, 'oe')
 			.replace(/ü/g, 'ue')
-			.replace(/ß/g, 'ss')
+			.replace(/ss/g, 'ss')
 			.replace(/[^a-z0-9]+/g, '-')
 			.replace(/^-+|-+$/g, '');
 	}
