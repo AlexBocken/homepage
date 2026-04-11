389a728d3ca4a785f0dab154c98d4f9dfde07a4e
Alexander Bocken <alexander@bocken.org>
2026-04-11 19:38:40 +0200

    feat: add chat.bocken.org link to homepage links grid

--- src/routes/(main)/+page.svelte
+++ src/routes/(main)/+page.svelte
@@ -53,6 +53,7 @@
 		shopping: isEnglish ? 'Shopping' : 'Einkauf',
 		familyTree: isEnglish ? 'Family Tree' : 'Stammbaum',
 		faith: isEnglish ? 'Faith' : 'Glaube',
+		chat: 'Chat',
 		transmission: 'Transmission',
 		documents: isEnglish ? 'Documents' : 'Dokumente',
 		audiobooksPodcasts: isEnglish ? 'Audiobooks & Podcasts' : 'Hörbücher & Podcasts',
@@ -199,6 +200,12 @@ section h2{
 	<a href="https://searx.bocken.org">
 		<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z"/></svg>
 		<h3>{labels.searchEngine}</h3>
+	</a>
+
+	<a href="https://chat.bocken.org">
+		<svg class="lock-icon"><use href="#lock-icon"/></svg>
+		<svg xmlns="http://www.w3.org/2000/svg" width="636" height="580" viewBox="-10 -258 636 580"><path d="M168-248h448v392L490 60v-161c0-12-9-21-21-21H301c-12 0-21 9-21 21s9 21 21 21h147v308H238l-126 84v-84H0V-80h168v-168z"/></svg>
+		<h3>{labels.chat}</h3>
 	</a>
 
 	<a href="https://tree.bocken.org">
